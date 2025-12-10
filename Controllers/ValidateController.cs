b4ecbbd0fd067cdbef7d7779dede4b5ed2719d68
bombombaixista <seuemail@exemplo.com>
2025-12-10 01:05:47 -0300

    Adicionado controller Validate para endpoint /api/validate

---
+++ Controllers/ValidateController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ValidateController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        // lógica de validação (ex: checar licença, token, etc.)
+        return Ok(new { status = "valid" });
+    }
+}
